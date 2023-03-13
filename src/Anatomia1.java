c958336b5a8cf8e4c6e6315fa7d9a73ea42d582a
Leandro <luizzleandro827@gmail.com>
2023-03-13 20:04:47 -0300

    Sintaxe Java Anatomia 1

---
+++ src/Anatomia1.java
@@ -0,0 +1,15 @@
+public class Anatomia1 {
+	public static void main(String[] args) {
+		
+		// Tipos de variáveis não podem ser violadas por exemplo um tipo int que significa inteiro não pode ser uma string;
+		
+		
+		double Decimal = 2.2;
+		int A = 2;
+		int Ano = 2023;
+		String BR = "Brasil";
+		System.out.println(BR + Ano);
+		System.out.println(Decimal + "." + A);
+	}
+}
+
